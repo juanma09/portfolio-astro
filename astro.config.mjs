9ca310b0b2f6e2dea268c1c2b09a5ba9bb81a4c5
juanma09 <mateo@elizeche.com>
2026-04-15 19:45:04 -0300

    Solve github link issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,15 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
+
   vite: {
     plugins: [tailwindcss()]
   },
